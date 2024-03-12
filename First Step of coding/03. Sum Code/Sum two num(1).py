d8168bb6f86223aad06b77e39d8a5c78c84b18be
TheGodVishnu21 <ishaansandhwar2106@gmail.com>
2024-03-13 01:43:56 +0530

    .

---
+++ First Step of coding/03. Sum Code/Sum two num(1).py
@@ -0,0 +1,9 @@
+#Addition of two string numbers
+a = input("Enter First Number:" )
+b = input("Enter Second Number:" )
+c = a + b
+print("Addition=", c) #Remember whenever we take "input" function will get output in str format. 
+
+# example: 
+# if will run the code and take a = 5 and b = 10 will get output 510. 
+# Means input function added numbers in string format "5" + "10".
---
+++ First Step of coding/03. Sum Code/Sum two num(2).py
@@ -0,0 +1,5 @@
+#Addition of two int numbers
+a = int(input("Enter First Number:" ))
+b = int(input("Enter Second Number:" ))
+c = a + b
+print("Addition=", c)
