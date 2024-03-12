b14570c40f5bfdb68aa98b9ca2b8bb07ee8bf9f4
TheGodVishnu21 <ishaansandhwar2106@gmail.com>
2024-03-12 20:01:02 +0530

    Assignment Operators

--- First Step of coding/02. Operators/Assignment.py
+++ First Step of coding/02. Operators/Assignment.py
@@ -1,4 +1,4 @@
-total = 0
+total = 0 
 total += 5  # Addition Assignment: total = total + 5
 total -= 2  # Subtraction Assignment: total = total - 2
 total *= 3  # Multiplication Assignment: total = total * 3
