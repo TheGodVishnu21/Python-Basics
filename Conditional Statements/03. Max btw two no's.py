146302881bae979b9be52c75cffe6eba3ddd2cf3
TheGodVishnu21 <ishaansandhwar2106@gmail.com>
2024-03-13 02:37:49 +0530

    max btw two no.

---
+++ Conditional Statements/03. Max btw two no's.py
@@ -0,0 +1,9 @@
+#Max between two numbers.
+a = int(input("Enter First Number: "))
+b = int(input("Enter Second Number: "))
+if a>b:
+    print("Max=", a)
+else:
+    print("Max=", b)
+    
+#Minimum is also same as this you can do it by your self.
