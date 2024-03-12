9c78f77e2c24f59b0d4e924d2d64231980e990fa
TheGodVishnu21 <ishaansandhwar2106@gmail.com>
2024-03-13 01:47:21 +0530

    sum of two integer numbers

--- First Step of coding/03. Sum Code/Sum two num(2).py
+++ First Step of coding/03. Sum Code/Sum two num(2).py
@@ -1,5 +1,5 @@
 #Addition of two int numbers
-a = int(input("Enter First Number:" ))
+a = int(input("Enter First Number:" )) #int function also known as integer which means will get output in number or integer format.
 b = int(input("Enter Second Number:" ))
 c = a + b
 print("Addition=", c)
