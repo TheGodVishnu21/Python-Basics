bf69eae0be3fdbaa3b2bdd710b372e9855e48669
TheGodVishnu21 <ishaansandhwar2106@gmail.com>
2024-03-13 22:42:38 +0530

    Max btw three numbers

---
+++ Conditional Statements/04. Max btw three no's.py
@@ -0,0 +1,13 @@
+#Max between three numbers.
+a = int(input("Enter First Number: "))
+b = int(input("Enter Second Number: "))
+c = int(input("Enter Third Number: "))
+if a>b and a>c:
+    print("Max=", a)
+elif b>a and b>c:
+    print("Max=", b)
+else:
+    print("Max=", c)
+
+#Minimum is also same as this you can do it by your self.
+#also find Mid number between three number
