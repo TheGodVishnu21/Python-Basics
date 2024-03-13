a354c5b1c579f446b4db5f7c5af0a8c04d2a0aea
TheGodVishnu21 <ishaansandhwar2106@gmail.com>
2024-03-14 02:02:13 +0530

    Count of Prime number.

---
+++ Looping Statements/02. Prime.py
@@ -0,0 +1,11 @@
+n = int(input("Enter No. to Check: "))
+count=0
+i=1
+while i<=n:
+    if n%i==0:
+        count+=1
+    i=i+1
+if count==2:
+    print("No. is Prime.")
+else:
+    print("No. is not Prime.")
